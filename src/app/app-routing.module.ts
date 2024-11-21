fa5fc697f7a2ea2c31b777b27f9c87d4fd0f57dc
DiegoST001 <diegosancheztafur3@gmail.com>
2024-11-20 23:36:05 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ListaEmpleadosComponent } from './lista-empleados/lista-empleados.component';
+import { RegistrarEmpleadoComponent } from './registrar-empleado/registrar-empleado.component';
+const routes: Routes = [
+  { path: 'empleados', component: ListaEmpleadosComponent }, // Corregido de 'empleado' a 'empleados'
+  { path: 'registrar-empleado', component: RegistrarEmpleadoComponent },
+  { path: '', redirectTo: 'empleados', pathMatch: 'full' }, // Redirección a empleados por defecto
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
